2ed8c84bd412699b286d25c227c5b7b319399669
Anthony Romrell <anthonyromrell@mac.com>
2019-08-29 15:39:24 -0600

    Worked on Syntax

---
+++ ProjectOne/Assets/Scripts/StartBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class StartBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        print("I love");
+        print("coding");
+        print("class");
+        print("and my Teacher."); 
+    }
+}
