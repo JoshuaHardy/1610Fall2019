5ac68ab846bf6e8fad9c340207958def8926b66b
Anthony Romrell <anthonyromrell@mac.com>
2019-09-17 15:58:04 -0600

    Character Controller.

---
+++ ProjectOne/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float gravity = 3f;
+    public float jumpSpeed = 10f;
+
+    private int jumpCount = 1;
+    
+    private Vector3 position;
+    public CharacterController controller;
+    void Update()
+    {
+        position.x = speed*Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded )
+        {
+            position.y -= gravity;
+        }
+        else
+        {
+            jumpCount = 1;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            position.y = jumpSpeed;
+            jumpCount--;
+        }
+        controller.Move(position*Time.deltaTime);
+    }
+}
